5bbe14ecf619b5e534a61e84ea3a946368aa2c83
Andrey Lavrusenko <andreylavrusenko@MacBook-Air-Andrey.local>
2022-01-08 22:33:26 +0300

    lib

--- js/modules/slider.js
+++ js/modules/slider.js
@@ -1,43 +1,51 @@
+
 const sliderSwipeHandler = (slides, prev, next) => {
-    let slideIndex = 1;
+    document.addEventListener("DOMContentLoaded", () => {
+        let slideIndex = 1;
 
-    const items = document.querySelectorAll(slides);
+        const items = document.querySelectorAll(slides);
 
-    const showSlides = n => {
-        if (n > items.length) {
-            slideIndex = 1;
+        const showSlides = n => {
+            if (n > items.length) {
+                slideIndex = 1;
+            }
+            
+            if (n < 1) {
+                slideIndex = items.length;
+            }
+
+            items.forEach(item => {
+                // item.classList.add('.animated');
+                // item.style.animationDuration = '2s';
+                item.style.display = 'none';
+            });
+
+            items[slideIndex - 1].style.display = "flex";  
+            items[slideIndex - 1].style.justifyContent = "space-around";
         }
-        
-        if (n < 1) {
-            slideIndex = items.length;
+
+        showSlides(slideIndex);
+
+        const plusSlides = n => {
+            showSlides(slideIndex += n);
         }
 
-        items.forEach(item => {
-            item.style.display = 'none';
+        const prevBtn = document.querySelector(prev),
+            nextBtn = document.querySelector(next);
+
+        prevBtn.addEventListener('click', () => {
+            plusSlides(-1);
         });
 
-        items[slideIndex - 1].style.display = "flex";  
-    }
 
-    showSlides(slideIndex);
-
-    const plusSlides = n => {
-        showSlides(slideIndex += n);
-    }
-
-    const prevBtn = document.querySelector(prev),
-          nextBtn = document.querySelector(next);
-
-    prevBtn.addEventListener('click', () => {
-        plusSlides(-1);
-    });
-
-
-    nextBtn.addEventListener('click', () => {
-        plusSlides(1);
-    });
-
+        nextBtn.addEventListener('click', () => {
+            plusSlides(1);
+        });
+    })
 }
 
 
+
+
 sliderSwipeHandler('.reviews_slider', '.slider_prev', '.slider_next');
+sliderSwipeHandler('.slider_group', '.slider_prev', '.slider_next');
